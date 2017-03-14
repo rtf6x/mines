e9c5f3a20b4b7941e3de3921ea78e2c96c842ff2
Alex Trapholov <trapholov@gmail.com>
2017-03-15 01:30:03 +0300

    webpack

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
-import { render } from 'react-dom'
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import App from './components/app.jsx'
-import reducer from './reducers'
+import React from 'react';
+import { render } from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './components/app.jsx';
+import reducer from './reducers';
+
+require("file-loader?name=index.html!./index.html");
+require('./css/main.scss');
 
 document.oncontextmenu = function () {
   return false;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
-var BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
 
 module.exports = {
   entry: './src/app.jsx',
   output: {
-    filename: 'build/app.js'
+    path: 'build',
+    filename: 'app.js'
   },
   module: {
     loaders: [
@@ -16,6 +21,62 @@ module.exports = {
         test: /\.js?$/,
         exclude: /(node_modules)/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.(scss)$/,
+        use: [{
+          loader: 'style-loader'
+        },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: false
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          },
+          {
+            loader: 'resolve-url-loader'
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              outputStyle: 'expanded',
+              sourceMap: true,
+              sourceMapContents: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(css)$/,
+        use: [{
+          loader: 'style-loader'
+        },
+          {
+            loader: 'resolve-url-loader'
+          }, {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 2,
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|png|ico|gif)$/,
+        use: [{
+          loader: 'url-loader',
+          options: {
+            limit: 10240 // Any png-image or woff-font below or equal to 10K will be converted to inline base64 instead
+          }
+        }]
       }
     ]
   },
@@ -24,6 +85,19 @@ module.exports = {
       host: 'localhost',
       port: 3000,
       server: { baseDir: ['build'] }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      test: /\.(scss|css)$/,
+      debug: true,
+      options: {
+        output: {
+          path: 'build'
+        },
+        postcss: [
+          autoprefixer({ browsers: 'last 2 version' })
+        ],
+        context: ''
+      }
     })
   ]
 };
